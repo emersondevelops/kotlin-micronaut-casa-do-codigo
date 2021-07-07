a0fd481c94f088b711a5584651ef97e17f572570
Emerson Bezerra <emersondevelops@gmail.com>
2021-07-07 14:46:10 -0300

    Implementa teste para cadastro de autor com uso de mock

---
+++ src/test/kotlin/br/com/zup/academy/autores/CadastraAutorControllerTest.kt
@@ -0,0 +1,54 @@
+package br.com.zup.academy.autores
+
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.test.annotation.MockBean
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.mockito.Mockito
+import javax.inject.Inject
+
+@MicronautTest
+internal class CadastraAutorControllerTest {
+
+    @field:Inject
+    lateinit var enderecoClient: EnderecoClient
+
+    @field:Inject
+    @field:Client("/")
+    lateinit var client: HttpClient
+
+    @Test
+    fun `Deve cadastrar um novo autor`() {
+
+        val novoAutorRequest = NovoAutorRequest(
+            "Emerson Bezerra",
+            "emerson.bezerra@zup.com.br",
+            "Participante do OT#5",
+            "58051-220",
+            "99"
+        )
+
+        val enderecoResponse = EnderecoResponse("Rua das Larajeiras", "Rio de Janeiro", "RJ")
+        Mockito.`when`(enderecoClient.consulta(novoAutorRequest.cep)).thenReturn(HttpResponse.ok(enderecoResponse))
+
+        val request = HttpRequest.POST("/autores", novoAutorRequest)
+        val response = client.toBlocking().exchange(request, Any::class.java)
+
+        assertEquals(HttpStatus.CREATED, response.status)
+        assertTrue(response.headers.contains("Location"))
+        assertTrue(response.header("Location")!!.matches("/autores/\\d+".toRegex()))
+    }
+
+    @MockBean(EnderecoClient::class)
+    fun enderecoMock(): EnderecoClient {
+        return Mockito.mock(EnderecoClient::class.java)
+    }
+}
